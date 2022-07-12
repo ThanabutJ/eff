5e487ca3da4f85f584d958638568b17b5c64f81b
thanabut.j <thanabut.j@lmwn.com>
2022-07-13 01:13:45 +0700

    add comments

--- README.md
+++ README.md
@@ -5,6 +5,12 @@
 - add unittest
   - [gomock](https://github.com/golang/mock)
 
-## project structure ref
+## project structure resources
 
-https://www.wolfe.id.au/2020/03/10/how-do-i-structure-my-go-project/
+- https://www.wolfe.id.au/2020/03/10/how-do-i-structure-my-go-project/
+
+## Heagonal Architecture resources
+
+aka. ports and adapters
+
+- https://medium.com/@matiasvarela/hexagonal-architecture-in-go-cfd4e436faa3
--- internal/repositories/product_repository.go
+++ internal/repositories/product_repository.go
@@ -1,16 +1,25 @@
 package repositories
 
-import "context"
+import (
+	"context"
+	"time"
+)
 
+// ProductInDB
+// Why there are more than 1 Product struct.
+// read https://medium.com/@matiasvarela/hexagonal-architecture-in-go-cfd4e436faa3
 type ProductInDB struct {
-	ID   string
-	Name string
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type ProductRepository interface {
 	FindAll(ctx context.Context) ([]*ProductInDB, error)
 }
 
+// productRepository
 type productRepository struct {
 }
 
@@ -18,6 +27,9 @@ func NewProductRepository() ProductRepository {
 	return &productRepository{}
 }
 
+// FindAll
+// We suppose to do actual DB query here.
+// However our struct dont have any DB connection setup yet. So I will just left if like this.
 func (r *productRepository) FindAll(ctx context.Context) ([]*ProductInDB, error) {
 	return []*ProductInDB{}, nil
 }
